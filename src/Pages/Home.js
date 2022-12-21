0dc548126adf830a37c4ebf7dec6ac78cab2fb12
Henry <aguhenrychuks@gmail.com>
2022-12-21 16:09:58 +0100

    feat:hero section of desktop home page, done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,15 +1,25 @@
 // stylesheet
-import './Home.css';
+import "./Home.css";
+
+// React Helmet
+import { Helmet } from "react-helmet";
 
 // Components
-import Hero from "../assets/Hero"
+import Hero from "../assets/Hero";
 
 const Home = () => {
   return (
-    <div className='home'>
-        <Hero/>
-    </div>
-  )
-}
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Catch Up | Welcome To Catch Up</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+      <div className="home">
+        <Hero />
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
--- src/assets/Hero.js
+++ src/assets/Hero.js
@@ -1,9 +1,12 @@
-import './Hero.css'
+import "./Hero.css";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="hero">
+      <h1>Having Fun with Friends</h1>
+      <p>Get fun stories, articles and news to talk about in your next hangout.</p>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
