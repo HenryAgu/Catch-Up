64e9248c03653c0958468807d4769a7f53b19774
Henry <aguhenrychuks@gmail.com>
2022-12-22 22:12:12 +0100

    feat:subscribe component added

--- src/assets/Subscribe.js
+++ src/assets/Subscribe.js
@@ -1,4 +1,8 @@
+// stylesheet
 import "./Subscribe.css";
+
+// Font awesome icon
+import { FaArrowRight } from "react-icons/fa";
 
 const Subscribe = () => {
   return (
@@ -7,7 +11,18 @@ const Subscribe = () => {
         <h1>SUBSCRIBE</h1>
       </div>
       <div className="inner-subscribe">
-        <div className="subscribe-email"></div>
+        <div className="subscribe-email">
+          <div className="email-box">
+            <input type="text" placeholder="Enter your Email Address" />
+            <FaArrowRight className="submit_email" />
+          </div>
+          <div className="email_info">
+            <p>
+              By providing your email, you indicate you have read and understood
+              our <span>privacy policy</span>
+            </p>
+          </div>
+        </div>
         <div className="subscribe-text">
           <p>
             Subscribe to receive the latest Fun blog posts and updates,
